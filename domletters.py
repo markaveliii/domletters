cd975e34aed36519599c658f735b570eb723d3cf
Mark Montes <mam25@pdx.edu>
2021-06-26 00:14:17 -0700

    solved domletters

--- domletters.py
+++ domletters.py
@@ -1,5 +1,33 @@
 #!/usr/bin/python3
 import sys
+from collections import Counter
 
-sentence = sys.stdin.read()
-print(sentence)
+def dlc(words):
+    if not words:
+        return 0
+    sum = 0
+    for word in words:
+        sum += count_dl(word)
+
+    return sum
+
+def count_dl(word):
+    if not word:
+        return 0
+    if not word.isalpha():
+        return 0
+
+    counted = Counter(word)
+    return max(counted.values())
+
+
+
+def main():
+    words = sys.stdin.read()
+    words = words.upper()
+    words = words.split()
+
+    print(dlc(words))
+
+if __name__ == "__main__":
+    main()
